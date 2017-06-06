3a53c1e972d5876a8bc82947e6259f5d21fab916
vmadmin <vmadmin@10.142.104.190>
2017-06-06 14:56:22 +0200

    asdf

--- WoWRetro/src/GUI/GUI.java
+++ WoWRetro/src/GUI/GUI.java
@@ -1,26 +1,50 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class GUI extends JFrame implements ActionListener {
+public class GUI extends JFrame implements ActionListener{
+
+	JButton JBStart=new JButton();
+	JButton JBEnd=new JButton();
 	
-	JButton JB1=new JButton();
-	JButton JB2=new JButton();
-	JButton JB3=new JButton();
-
-	public static void main(String[] args) {
-
+	public GUI() {
 		
-	} 
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
+		this.setLayout(null);
+		this.setSize(1280,720);
+		this.setVisible(true);
+		this.setForeground(Color.BLUE);
+		
+		JBStart.setBounds(540, 200, 200, 50);
+		JBEnd.setBounds(540, 270, 200, 50);
+		
+		JBStart.setText("Starten");
+		JBEnd.setText("Beenden");
+		
+		JBStart.addActionListener(this);
+		JBEnd.addActionListener(this);
 		
 		
+		this.add(JBStart);
+		this.add(JBEnd);
+		
+		
+	
 	}
 
+	
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource()==JBStart){
+			
+		}
+		if(event.getSource()==JBEnd){
+			this.dispose();
+		}
+	}
 }
