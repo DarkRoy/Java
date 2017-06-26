423e1e5bdae6b4adc48869546b39c3cc43883335
vmadmin <vmadmin@bmWP2.fritz.box>
2017-06-26 20:23:57 +0200

    fafas

--- WoWRetro/src/Data/EnemyList.java
+++ WoWRetro/src/Data/EnemyList.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 
 import Logic.IEnemy;
 
+
+
 public class EnemyList {
+	
  
+	/** 
+	 * Speichert IEnemy Objekte in die ArrayList
+	 */
 	private ArrayList<IEnemy> EnemyList=new ArrayList<IEnemy>();
 	
 	public EnemyList() {
 		
 		
 	}
+	/**
+	 * Diese Methode speichert IEnemy Objekte in die ArrayList EnemyList
+	 * @param Enemy Variable mit dem Datentyp IEnemy
+	 */
 	public void AddEnemy(IEnemy Enemy) {
 		EnemyList.add(Enemy);
 	} 
