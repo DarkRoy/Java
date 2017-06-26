e3bb5ae2e86fa1980792309202d8f04e27964bc2
vmadmin <vmadmin@bmWP2.fritz.box>
2017-06-26 21:19:02 +0200

    bsd<w

--- WoWRetro/src/Logic/IEnemy.java
+++ WoWRetro/src/Logic/IEnemy.java
@@ -11,8 +11,8 @@ public interface IEnemy {
 	 */
 	public int getHP();
 	/**
-	 * 
-	 * @param HP ist eine Variable des Datentyps int und bestimmt den Lebenswert
+	 * Diese Methode bestimmt den Lebenswert
+	 * @param HP ist eine Variable des Datentyps int
 	 */
 	public void setHP(int HP);
 	
@@ -22,15 +22,40 @@ public interface IEnemy {
 	 */
 	public int getATK();
 	/**
-	 * 
-	 * @param ATK ist eine Variable des Datentyps int und  bestimmt den Schadenswert
+	 * Diese Methode bestimmt den Schadenswert
+	 * @param ATK ist eine Variable des Datentyps int
 	 */
 	public void setATK(int ATK);
+	/**
+	 * 
+	 * @return gibt den Namen zurück
+	 */
 	public String getName();
+	/**
+	 * Diese Methode setzt den Namen
+	 * @param Name ist eine Variable des Datentyps String
+	 */
 	public void setName(String Name);
+	/**
+	 * 
+	 * @return gibt den maximalen Anfangswerte der Lebenswerte zurück
+	 */
 	public int getMAXHP();
+	/**
+	 * Diese Methode setzt die Angangswerte für die Lebenswerte
+	 * @param MAXHP ist eine Variable des Datentyps int
+	 */
 	public void setMAXHP(int MAXHP);
+	/**
+	 * 
+	 * @param ATK ist eine Variable des Datentyps int
+	 * @param AA ist eine Variable des Datentyps int
+	 */
 	void DamageDealer(int ATK, int AA);
+	/**
+	 * 
+	 * @return gibt den zugefügten Schaden zurück
+	 */
 	public int getDamageRec();
 
 }
